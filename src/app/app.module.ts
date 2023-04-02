aa14e7efaa1f6e1bc54a8f61074da01cad46a145
LuisCldrn <Luis.cldrn@yahoo.com>
2023-04-02 13:22:41 -0500

    ADDED OKTA LOGIN FUNCTIONALITY added OKta login login status login page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,8 +14,18 @@ import { CartStatusComponent } from './components/cart-status/cart-status.compon
 import { CartDetailsComponent } from './components/cart-details/cart-details.component';
 import { CheckoutComponent } from './components/checkout/checkout.component';
 import { ReactiveFormsModule } from '@angular/forms';
+import { LoginComponent } from './components/login/login.component';
+import { LoginStatusComponent } from './components/login-status/login-status.component';
+import { OktaAuthModule, OktaCallbackComponent, OKTA_CONFIG } from '@okta/okta-angular';
+import {OktaAuth} from '@okta/okta-auth-js'
+import myAppConfig from './config/my-app-config';
+
+const oktaConfig = myAppConfig.oidc;
+const oktaAuth = new OktaAuth(oktaConfig);
 
 const routes: Routes = [
+  {path: 'login', component: LoginComponent},
+  {path: 'login/callback', component: OktaCallbackComponent},
   {path: 'products/:id', component: ProductDetailsComponent},
   {path: 'checkout', component: CheckoutComponent},
   {path: 'search/:keyword', component: ProductListComponent},
@@ -38,6 +48,8 @@ const routes: Routes = [
     CartStatusComponent,
     CartDetailsComponent,
     CheckoutComponent,
+    LoginComponent,
+    LoginStatusComponent,
   ],
   imports: [
     RouterModule.forRoot(routes),
@@ -45,8 +57,9 @@ const routes: Routes = [
     HttpClientModule,
     NgbModule,
     ReactiveFormsModule,
+    OktaAuthModule,
   ],
-  providers: [ProductService],
+  providers: [ProductService, {provide: OKTA_CONFIG, useValue: {oktaAuth}}],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/config/my-app-config.ts
@@ -0,0 +1,8 @@
+export default {
+    oidc: {
+        clientId: '0oa8yam9hyPNL8rg35d7',
+        issuer: 'https://dev-53169467.okta.com/oauth2/default',
+        redirectUri: 'http://localhost:4200/login/callback',
+        scopes: ['openid', 'profile', 'email']
+    }
+}
