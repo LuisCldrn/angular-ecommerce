10acc811e996d5c8a8e2f16618412bac42d925fd
LuisCldrn <Luis.cldrn@yahoo.com>
2023-04-03 20:50:32 -0500

    implemented header for oktaAuth

---
+++ src/app/services/auth-interceptor.service.ts
@@ -0,0 +1,40 @@
+import { HttpEvent, HttpHandler, HttpInterceptor, HttpRequest } from '@angular/common/http';
+import { Inject, Injectable } from '@angular/core';
+import { OKTA_AUTH } from '@okta/okta-angular';
+import OktaAuth from '@okta/okta-auth-js';
+import { from, lastValueFrom, Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthInterceptorService implements HttpInterceptor{
+
+  
+
+  constructor(
+    @Inject(OKTA_AUTH) private oktaAuth: OktaAuth,
+  ) { }
+  intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+    return from(this.handleAccess(request, next))
+  }
+  private async handleAccess(request: HttpRequest<any>, next: HttpHandler): Promise<HttpEvent<any>> {
+    //only for secure endpoints
+
+    const securedEndpoints = ['http://localhost:8080/api/orders'];
+
+    if (securedEndpoints.some(url => request.urlWithParams.includes(url))) {
+      const accessToken = this.oktaAuth.getAccessToken();
+
+      //clone request to add new header
+
+      request = request.clone({
+        setHeaders: {
+          Authorization: 'Bearer ' + accessToken
+        }
+      });
+    }
+
+    return await lastValueFrom(next.handle(request));
+
+  }
+}
