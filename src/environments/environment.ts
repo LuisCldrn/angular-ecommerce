fdae578f5e655a4d3eb9844a1a871f158e0db6ec
LuisCldrn <Luis.cldrn@yahoo.com>
2023-04-19 19:52:59 -0500

    updated aws

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
     production: true,
-    luv2shopApiUrl: "http://localhost:8080/api",
+    luv2shopApiUrl: "http://e-commerce-env.eba-gdwa7si2.us-east-1.elasticbeanstalk.com/api",
     stripePublishableKey: "pk_test_51MtDEjHBvtw6BF0Z0vIwS3jiCiBfGvEOzetcxNmaJ0lNHtgpCG7u4Y11hTrL5qAHhHVyAIkWZHRwdQzjE47KuedN00BVYhjoal"
 
 };
